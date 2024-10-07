b39d95ad10365bc9cac1295dacdc4b12e5a4dd99
Patryk Piątek <patryk@witbee.com>
2024-10-07 11:07:18 +0200

    Init

---
+++ app/components/content/HomeFeature.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex items-start gap-3">
+    <div>
+      <UIcon :name="icon" />
+    </div>
+
+    <div class="flex flex-col">
+      <h3 class="text-lg font-semibold">
+        <ContentSlot :name="title" />
+      </h3>
+      <span>
+        <ContentSlot :name="description" />
+      </span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  icon: {
+    type: String,
+    default: "i-ph-cursor-click",
+  },
+});
+</script>
